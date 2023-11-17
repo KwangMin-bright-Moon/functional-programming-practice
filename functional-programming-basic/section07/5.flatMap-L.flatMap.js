d374fd82dd37143ded3ac2cc5fc48ab7e2cad44f
Jason <jasondal91@gmail.com>
2023-11-17 23:18:47 +0900

    section08

--- functional-programming-basic/section07/5.flatMap-L.flatMap.js
+++ functional-programming-basic/section07/5.flatMap-L.flatMap.js
@@ -1,4 +1,4 @@
-import { curry, pipe, L } from '../utils.js';
+import { curry, pipe, L, flatten, log, map } from '../utils.js';
 
 L.flatMap = curry(pipe(L.map, L.flatten));
 
@@ -22,3 +22,44 @@ log(
     map((a) => a + 1, [1, 2, 3])
   )
 );
+
+const company = {
+  회사명: '스터디캠프',
+  설립일: '2023-05-05',
+  기업구성원: {
+    인원수: 4,
+    명단: ['코치', '초롱이', '마동석', '빡빡이'],
+  },
+  분야: '통신업',
+};
+
+const flattenCompany = (company) => {
+  return {
+    ...company,
+    ...company['기업구성원'],
+  };
+};
+
+const transformCompany = pipe(flattenCompany, ({ 기업구성원, ...rest }) => {
+  return {
+    ...rest,
+  };
+});
+
+log(transformCompany(company));
+
+const newFlatten = (arr) => {
+  let result = [];
+
+  for (const a of arr) {
+    if (Array.isArray(a)) {
+      result = result.concat(flatten(a));
+    } else {
+      result.push(a);
+    }
+  }
+
+  return result;
+};
+
+log(newFlatten([1, [2, 4]]));
